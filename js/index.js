b8769d3cf216debcd6317e29b4555e1d34a53796
Neha Anand <neha280103@gmail.com>
2022-03-10 15:05:42 +0530

    added hero section

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 let navToggle = document.querySelector(".nav__toggle");
 let navWrapper = document.querySelector(".nav__wrapper");
 let hamburgerIcon = document.querySelector(".icon");
+let heroImg = document.querySelector(".hero-img");
 
 navToggle.addEventListener("click", function () {
   if (navWrapper.classList.contains("active")) {
@@ -9,11 +10,13 @@ navToggle.addEventListener("click", function () {
     navWrapper.classList.remove("active");
     hamburgerIcon.classList.remove("fa-times");
     hamburgerIcon.classList.add("fa-bars");
+    heroImg.classList.remove("turn-off");
 
     
   } else {
     hamburgerIcon.classList.add("fa-times");
     navWrapper.classList.add("active");
+    heroImg.classList.add("turn-off");
     this.setAttribute("aria-label", "close menu");
     this.setAttribute("aria-expanded", "true");
     hamburgerIcon.classList.remove("fa-bars");
