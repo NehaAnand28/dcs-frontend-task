37ef9825574bef9ed5f52ce42f1e2733c17a0312
Neha Anand <neha280103@gmail.com>
2022-03-16 19:47:50 +0530

    added js form validation

--- js/index.js
+++ js/index.js
@@ -11,8 +11,6 @@ navToggle.addEventListener("click", function () {
     hamburgerIcon.classList.remove("fa-times");
     hamburgerIcon.classList.add("fa-bars");
     heroImg.classList.remove("turn-off");
-
-    
   } else {
     hamburgerIcon.classList.add("fa-times");
     navWrapper.classList.add("active");
@@ -21,9 +19,7 @@ navToggle.addEventListener("click", function () {
     this.setAttribute("aria-expanded", "true");
     hamburgerIcon.classList.remove("fa-bars");
   }
-
 });
-
 
 //slider
 let items = document.querySelectorAll(".carousel .carousel-item");
@@ -41,3 +37,45 @@ items.forEach((el) => {
     next = next.nextElementSibling;
   }
 });
+
+//client side form validation
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validateInputs();
+});
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = message;
+  inputControl.classList.add("error");
+  inputControl.classList.remove("success");
+};
+
+const setSuccess = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector(".error");
+
+  errorDisplay.innerText = "";
+  inputControl.classList.add("success");
+  inputControl.classList.remove("error");
+};
+
+const isValidEmail = (email) => {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+const validateInputs = () => {
+  const emailValue = email.value.trim();
+
+  if(emailValue === ""){
+    setError(email,"Email is required");
+  }else if(!isValidEmail(emailValue)){
+    setError(email,"Please insert a valid email");
+  }else setSuccess(email);
+};
